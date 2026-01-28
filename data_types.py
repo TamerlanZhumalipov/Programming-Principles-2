ede7aebe5598255fd61dbb2eb6c4cafd9364ced9
TamerlanZhumalipov <966h7n95zv@privaterelay.appleid.com>
2026-01-28 12:44:43 +0500

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,17 @@
+x = 5
+print(type(x)) #int
+
+c = "aaa"
+print(type(c)) #str
+
+b = ["apple", "banana", "cherry"]
+print(type(b)) #list
+
+a = 20.5
+print(type(a)) #float
+
+d = ("apple", "banana", "cherry")
+print(type(a)) #tuple
+
+
+
---
+++ numbers.py
@@ -0,0 +1,45 @@
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+x = 1 #int
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 1.10 #float
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 3+5j #complex
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
